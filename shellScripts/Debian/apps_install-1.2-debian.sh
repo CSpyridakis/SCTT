e6141da678ab5f50c0009b3bc8164e3a4d795afd
Spyridakis Christos <spyridakischristos@gmail.com>
2021-07-18 11:21:14 +0300

    ncdu installation added

--- shellScripts/Debian/apps_install-1.2-debian.sh
+++ shellScripts/Debian/apps_install-1.2-debian.sh
@@ -420,7 +420,8 @@ system(){
     __INST pasystray                                  #PulseAudio system tray
     __INST alltray                                    #Dock apps in system tray (GNOME)
     __INST python3-pip 
-    
+    __INST ncdu                                       #Disk space monitoring tool
+
     __INST paprefs                                    #Enable multiple audio outputs at once
     #To work, install it, open it from terminal goto Simultaneous Outputs and enable it, then
     #run 'pulseaudio -k' and you are ready
