b98d457123d675d304819480c13761a4722e1b79
Spyridakis Christos <spyridakischristos@gmail.com>
2021-11-14 19:22:49 +0200

    massive rename script add input arguments

--- shellScripts/Bash-automate-actions/massive-rename-files-in-folder.sh
+++ shellScripts/Bash-automate-actions/massive-rename-files-in-folder.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 
-PRE="img"
+DIR=""
+PRE="image"
 EXT="png"
 
-i=0 ; 
-for file in `find . -name \*.${EXT} | sort -V` ; do  
-    ((i=i+1))
-    echo "${file} ${PRE}-$i.${EXT}"
-    mv -f ${file} ${PRE}-$i.${EXT}
+rename_files(){
+    i=0 ; 
+    for file in `find ./${DIR} -maxdepth 1 -name \*.${EXT} | sort -V` ; do  
+        ((i=i+1))
+        echo "mv -f ${file} ./${DIR}${PRE}-$i.${EXT}"
+        # mv -f ${file} ./${DIR}/${PRE}-$i.${EXT}
+    done
+}
+
+while :
+do
+    case "$1" in
+        -d | --dir)         DIR="$2/"  ;   shift ; shift ;;
+        -f | --file-name)   PRE="$2"  ;   shift ; shift ;;
+
+        --*)
+            echo "Unknown option: $1" >&2
+            helpMenu
+            exit 1
+            ;;
+        -*)
+            echo "Unknown option: $1" >&2
+            helpMenu
+            exit 1 
+            ;;
+        *) 
+            break
+    esac
 done
+
+rename_files
