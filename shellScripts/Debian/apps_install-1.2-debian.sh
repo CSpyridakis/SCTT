13e693f551b94c11dcaeeee365e1026e46d35edd
Spyridakis Christos <spyridakischristos@gmail.com>
2023-12-27 19:02:36 +0200

    Debian change default file browser

--- shellScripts/Debian/apps_install-1.2-debian.sh
+++ shellScripts/Debian/apps_install-1.2-debian.sh
@@ -426,6 +426,8 @@ system(){
     __INST ranger                                     #CLI file manager
     __INST nload                                      #Keep an eye on network traffic and bandwidth usage
 
+    __INST xfce4-settings                             #To replace default file browser (run xfce4-mime-settings and goto utils)
+    
     __INST paprefs                                    #Enable multiple audio outputs at once
     #To work, install it, open it from terminal goto Simultaneous Outputs and enable it, then
     #run 'pulseaudio -k' and you are ready
